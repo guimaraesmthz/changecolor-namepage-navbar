6ef80eb3f4193e0f29a666a5ecc79ec48d62469a
Matheus Guimarães <91638850+guimaraesmthz@users.noreply.github.com>
2021-12-06 23:48:20 -0300

    Update changecolornav.js

--- changecolornav.js
+++ changecolornav.js
@@ -1,9 +1,10 @@
 
 
 
-const pageAtual = window.location.href.split('/')[8];
+const pageAtual = window.location.href.split('/')[8]; // Here it takes the address of the page and separates it into smaller pieces where there is "/". To select just the filename of the current page, count your address and change the number.
 
-const page = document.querySelector(`[href="${pageAtual}"]`)
 
-page.style.color="#f2cb05";
+const page = document.querySelector(`[href="${pageAtual}"]`) // Here it selects the href whose value is the name of the current page.
+
+page.style.color="#f2cb05"; // Here it changes the color property.
 
